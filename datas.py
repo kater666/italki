7d23ec65d13bc4d8cff035b5501431300ad03be5
kater666 <michal.dolbniak@gmail.com>
2016-03-22 16:07:09 +0100

    Fixed data storing problem.

--- datas.py
+++ datas.py
@@ -1,14 +1,31 @@
 import xlrd
 
-
+# Variables:
 file_location = "./datas.xlsx"
 workbook = xlrd.open_workbook(file_location)
+
+# Users' sheet
 users = workbook.sheet_by_index(0)
+u_columns = users.ncols
+u_rows = users.nrows
+data_users = [[users.cell_value(r, c) for c in range(u_columns)] for r in range(1, u_rows)]
+
+# Languages' sheet
 languages = workbook.sheet_by_index(1)
+l_columns = languages.ncols
+l_rows = languages.nrows
+data_languages = [languages.cell_value(r, 0) for r in range(1, l_rows)]
 
-data_users = [[users.cell_value(r, c) for c in range(users.ncols)] for r in range(1, users.nrows)]
-data_languages = [[languages.cell_value(r, c) for c in range(languages.ncols)] for r in range(1, languages.nrows)]
+
+def get_email(name):
+    for i in data_users:
+        if name == i[1]:
+            return i[2]
 
 
-# zrobić plik excelowski z użytkownikami i językami
-# metody do pozyskania emaila i hasła
+def get_password(name):
+    for i in data_users:
+        if name == i[1]:
+            return i[4]
+
+print(data_languages)
--- pages.py
+++ pages.py
@@ -1,2 +1,5 @@
 from base import Page
 from locators import *
+
+# sprawdź języki
+# zaloguj się i coś tam odjeb
